3e463077c3f894cfea0e7bc1b8469c535bcaf607
hthakur <97741242+hthakur1@users.noreply.github.com>
2022-12-09 16:39:45 +0530

    updated headers

--- Woopons/Api Client/CustomHeaders.swift
+++ Woopons/Api Client/CustomHeaders.swift
@@ -17,12 +17,10 @@ class CustomHeaders {
             "Content-Type" : "application/json",
         ]
 
-//        if let authToken = UserDefaults.standard.string(forKey: "accessToken") {
-//            headers["Authorization"] = "Bearer" + " " + authToken
-//        }
-        headers["Authorization"] = "Bearer" + " " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3dvb3BvbnMueGNlbGFuY2V3ZWIuY29tL2FwaS92MS9hdXRoL2xvZ2luIiwiaWF0IjoxNjcwMjU2NzYzLCJuYmYiOjE2NzAyNTY3NjMsImp0aSI6IkN2ZmF2T0J0SGxPZTRhWE4iLCJzdWIiOiIxNiIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Mo9pgk-mFqf6q9lC1gpYr9rf6GzH-8MTe5xm8EkIPzA"
-       
-
+        if let authToken = UserDefaults.standard.string(forKey: "accessToken") {
+            headers["Authorization"] = "Bearer" + " " + authToken
+        }
+    
         return headers
     }
 }
