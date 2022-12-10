315616840a25da839acc68afe2c371ee3559f834
hthakur <97741242+hthakur1@users.noreply.github.com>
2022-12-11 00:14:43 +0530

    Unlock coupon fixed

--- Woopons/ViewControllers/UnlockCouponViewController.swift
+++ Woopons/ViewControllers/UnlockCouponViewController.swift
@@ -11,6 +11,7 @@ class UnlockCouponViewController: UIViewController {
     
     @IBOutlet weak var dottedView: UIView!
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var popUpView: UIView!
     @IBOutlet weak var couponCode: UILabel!
     
     var titleString = ""
@@ -21,13 +22,24 @@ class UnlockCouponViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setBackButtonWithTitle(title: "")
+       // self.setBackButtonWithTitle(title: "")
         self.title =  titleString
         dottedView.addDashedBorder()
         self.couponCode.text = coupon
         self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        self.unlockCoupon()
+        //self.unlockCoupon()
+        let closeButtonImage = UIImage(named: "back")?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: closeButtonImage, style: .plain, target: self, action:  #selector(UnlockCouponViewController.barButtonDidTap(_:)))
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func barButtonDidTap(_ sender: UIBarButtonItem)
+    {
+        self.popUpView.isHidden = false
+    }
+    
+    @IBAction func okTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
     }
     
     @objc func updateCounter() {
